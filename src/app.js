7e502b58e8612a3193322da1ed6983a197435f9f
Juan Nebbia <juan.nebbia@gmail.com>
2025-03-29 12:27:18 -0300

    Set up db connection and Sequelize configuration

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 import express from "express";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000
 
-app.listen(PORT, ()=> console.log('Server listening on port', PORT));
+connectDB()
+
+app.listen(PORT, ()=> console.log(' Server listening on port', PORT));
---
+++ src/config/db.js
@@ -0,0 +1,36 @@
+import dotenv from "dotenv"
+import { Sequelize } from "sequelize";
+
+dotenv.config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    logging: false,
+  }
+);
+
+const connectDB = async(retries = 3, delay = 3000) => {
+    while(retries){
+        try {
+            await sequelize.authenticate()
+            console.log("DB connected successfully")
+            return
+        } catch (err) {
+            console.log("Error connecting to DB:", err.message)
+            retries -=1
+            if(!retries){
+                console.log("DB connection failed:", err.message)
+                process.exit(1)
+            }
+            console.log("Retrying connection...")
+            await new Promise((res) => setTimeout(res, delay));
+        }
+    }
+}
+
+export { sequelize, connectDB };
